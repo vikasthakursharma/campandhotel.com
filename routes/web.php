f66e7ca8ff314d2b9caab6aeb4293b0f433b6730
yogender1995 <42475357+yogender1995@users.noreply.github.com>
2022-05-02 12:33:32 +0530

    update previous changes
    update previous changes

--- routes/web.php
+++ routes/web.php
@@ -96,9 +96,6 @@ Route::get('/not-access', function () {
     return view('backend.layouts.errors-404');
 });
 
-Route::get('/testhost', function() {
-    echo "working testhost";
-});
 Route::middleware(['active.login'])->group(function () {
     // ----- no access without login -----------//
 Route::middleware(['admin.guard'])->group(function () {
